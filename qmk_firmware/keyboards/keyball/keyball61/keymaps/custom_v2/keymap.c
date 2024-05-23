26eea3abbd7c18a13b74243b592fc550d30c7163
brianliu <hong86323@gmail.com>
2024-05-23 12:51:48 +0800

    clone custom keymap to custom_v2

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
@@ -20,30 +20,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+enum Layers {
+    base = 0,
+    nav,
+    mouse,
+    media
+};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [base] = LAYOUT_universal(
-    KC_GRV        , KC_1      , KC_2           , KC_3          , KC_4          , KC_5           ,                                             KC_6    , KC_7     , KC_8     , KC_9     , KC_0        , KC_MINS  ,
-    KC_TAB        , KC_Q      , KC_W           , KC_E          , KC_R          , KC_T           ,                                             KC_Y    , KC_U     , KC_I     , KC_O     , KC_P        , KC_EQL   ,
-    CW_TOGG       , KC_A      , KC_S           , KC_D          , KC_F          , KC_G           ,                                             KC_H    , KC_J     , KC_K     , KC_L     , KC_SCLN     , KC_QUOT  ,
-    OSM(MOD_LSFT) , KC_Z      , KC_X           , KC_C          , KC_V          , KC_B           , KC_RBRC          ,       KC_RBRC          , KC_N    , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH     , KC_PIPE  ,
-    OSM(MOD_LCTL) , C(KC_SPC) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , LT(nav,KC_SPC) , LT(mouse,KC_ESC) ,       LT(media,KC_ENT) , KC_BSPC , KC_NO    , KC_NO    , KC_NO    , OSM(KC_MEH) , SCMD(KC_2)
+    KC_GRV        , KC_1      , KC_2           , KC_3          , KC_4          , KC_5           ,                                              KC_6   , KC_7     , KC_8     , KC_9     , KC_0        , KC_MINS  ,
+    KC_TAB        , KC_Q      , KC_W           , KC_E          , KC_R          , KC_T           ,                                              KC_Y   , KC_U     , KC_I     , KC_O     , KC_P        , KC_EQL   ,
+    CW_TOGG       , KC_A      , KC_S           , KC_D          , KC_F          , KC_G           ,                                              KC_H   , KC_J     , KC_K     , KC_L     , KC_SCLN     , KC_QUOT  ,
+    OSM(MOD_LSFT) , KC_Z      , KC_X           , KC_C          , KC_V          , KC_B           , KC_RBRC          ,       KC_RBRC           , KC_N   , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH     , KC_PIPE  ,
+    OSM(MOD_LCTL) , C(KC_SPC) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , LT(nav,KC_SPC) , LT(mouse,KC_ESC) ,       LT(media,KC_BSPC) , KC_ENT , KC_NO    , KC_NO    , KC_NO    , OSM(KC_MEH) , SCMD(KC_2)
   ),
 
   [nav] = LAYOUT_universal(
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , KC_NO      , KC_NO      , KC_NO       , KC_NO       , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , G(KC_LEFT) , A(KC_LEFT) , A(KC_RIGHT) , G(KC_RIGHT) , KC_NO    ,
-    KC_NO    , MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , KC_NO    ,                                 KC_RIGHT , KC_DOWN    , KC_UP      , KC_LEFT     , KC_NO       , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           KC_NO    , KC_NO    , KC_HOME    , KC_PGDN    , KC_PGUP     , KC_END      , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , _______  , KC_NO    ,           KC_NO    , KC_NO    , _______    , KC_NO      , KC_NO       , KC_NO       , KC_NO
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO      , KC_NO      , KC_NO       , KC_NO       , KC_NO    , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 G(KC_LEFT) , A(KC_LEFT) , A(KC_RIGHT) , G(KC_RIGHT) , KC_NO    , KC_NO    ,
+    KC_NO    , MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , KC_NO    ,                                 KC_LEFT    , KC_DOWN    , KC_UP       , KC_RIGHT    , KC_NO    , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           KC_NO    , KC_NO      , KC_HOME    , KC_PGDN     , KC_PGUP     , KC_END   , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , _______  , KC_NO    ,           KC_NO    , KC_NO      , _______    , KC_NO       , KC_NO       , KC_NO    , KC_NO
   ),
 
   [mouse] = LAYOUT_universal(
     KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO   , KC_NO    ,
     KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO   , KC_NO    ,
-    KC_NO    , MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , KC_NO    ,                                 KC_BTN2  , KC_BTN1  , KC_BTN3  , KC_LEFT  , KC_NO   , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO   , KC_NO    ,
+    KC_NO    , MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , AML_TO   ,                                 KC_BTN2  , KC_BTN1  , KC_BTN3  , KC_LEFT  , KC_NO   , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           KC_NO    , KC_NO    , CPI_D100 , CPI_I100 , KC_NO    , KC_NO   , KC_NO    ,
     KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , _______  ,           KC_NO    , KC_NO    , _______  , KC_NO    , KC_NO    , KC_NO   , KC_NO
   ),
 
@@ -62,14 +68,3 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
     return state;
 }
-
-#ifdef OLED_ENABLE
-
-#    include "lib/oledkit/oledkit.h"
-
-void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
-}
-#endif
