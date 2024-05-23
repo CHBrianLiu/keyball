4ab98bd1c94abdc81a73ed4aade8aa15272eba25
brianliu <hong86323@gmail.com>
2024-05-24 00:40:38 +0800

    set the default keyball speed

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/config.h
@@ -37,3 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
+
+
+#define KEYBALL_CPI_DEFAULT 500
