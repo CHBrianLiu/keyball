db1e1fdf205cafa492c2003e14b5dc84481fa555
brianliu <hong86323@gmail.com>
2024-05-24 21:54:04 +0800

    use my right palm to enable scrolling temporarily

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB        , KC_Q      , KC_W           , KC_E          , KC_R          , KC_T           ,                                              KC_Y   , KC_U     , KC_I     , KC_O     , KC_P        , KC_EQL   ,
     CW_TOGG       , KC_A      , KC_S           , KC_D          , KC_F          , KC_G           ,                                              KC_H   , KC_J     , KC_K     , KC_L     , KC_SCLN     , KC_QUOT  ,
     OSM(MOD_LSFT) , KC_Z      , KC_X           , KC_C          , KC_V          , KC_B           , KC_LBRC          ,       KC_RBRC           , KC_N   , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH     , KC_BSLS  ,
-    G(KC_TAB)     , C(KC_SPC) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , LT(nav,KC_SPC) , LT(mouse,KC_ESC) ,       LT(media,KC_BSPC) , KC_ENT , KC_NO    , KC_NO    , KC_NO    , OSM(KC_MEH) , SCMD(KC_2)
+    G(KC_TAB)     , C(KC_SPC) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , LT(nav,KC_SPC) , LT(mouse,KC_ESC) ,       LT(media,KC_BSPC) , KC_ENT , KC_NO    , KC_NO    , KC_NO    , OSM(KC_MEH) , SCRL_MO
   ),
 
   [nav] = LAYOUT_universal(
@@ -46,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [mouse] = LAYOUT_universal(
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KBC_RST  , KBC_SAVE   , KC_NO      , KC_NO    , KC_NO   , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , G(KC_LBRC) , G(KC_RBRC) , KC_NO    , KC_NO   , KC_NO    ,
-    KC_NO    , MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , AML_TO   ,                                 KC_BTN2  , KC_BTN1    , KC_BTN3    , SCRL_TO  , KC_NO   , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           KC_NO    , KC_NO    , CPI_D100   , CPI_I100   , KC_NO    , KC_NO   , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , _______  ,           G(KC_W)  , KC_NO    , _______    , KC_NO      , KC_NO    , KC_NO   , KC_NO
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KBC_RST  , KBC_SAVE   , KC_NO      , KC_NO    , KC_NO    , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , G(KC_LBRC) , G(KC_RBRC) , KC_NO    , KC_NO    , KC_NO    ,
+    KC_NO    , MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , AML_TO   ,                                 KC_BTN2  , KC_BTN1    , KC_BTN3    , KC_NO    , KC_NO    , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           KC_NO    , KC_NO    , CPI_D100   , CPI_I100   , SCRL_DVI , SCRL_DVD , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , _______  ,           G(KC_W)  , KC_NO    , _______    , KC_NO      , KC_NO    , KC_NO    , _______
   ),
 
   [media] = LAYOUT_universal(
