cc19bf5ade6ea479c3f388f704ae5fb5c3197fe5
brianliu <hong86323@gmail.com>
2024-05-28 00:29:44 +0800

    adjust mouse layer

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
@@ -33,11 +33,11 @@ enum Layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [base] = LAYOUT_universal(
-    KC_GRV        , KC_1      , KC_2           , KC_3          , KC_4          , KC_5           ,                                              KC_6   , KC_7     , KC_8     , KC_9     , KC_0         , KC_MINS  ,
-    KC_TAB        , KC_Q      , KC_W           , KC_E          , KC_R          , KC_T           ,                                              KC_Y   , KC_U     , KC_I     , KC_O     , KC_P         , KC_EQL   ,
-    CW_TOGG       , KC_A      , KC_S           , KC_D          , KC_F          , KC_G           ,                                              KC_H   , KC_J     , KC_K     , KC_L     , KC_SCLN      , KC_QUOT  ,
-    OSM(MOD_LSFT) , KC_Z      , KC_X           , KC_C          , KC_V          , KC_B           , KC_LBRC          ,       KC_RBRC           , KC_N   , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH      , KC_BSLS  ,
-    G(KC_TAB)     , C(KC_SPC) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , LT(nav,KC_SPC) , LT(mouse,KC_ESC) ,       LT(media,KC_BSPC) , KC_ENT , KC_NO    , KC_NO    , KC_NO    , OSM(MOD_MEH) , SCRL_MO
+    KC_GRV        , KC_1      , KC_2           , KC_3          , KC_4          , KC_5           ,                                              KC_6   , KC_7     , KC_8     , KC_9     , KC_0    , KC_MINS  ,
+    KC_TAB        , KC_Q      , KC_W           , KC_E          , KC_R          , KC_T           ,                                              KC_Y   , KC_U     , KC_I     , KC_O     , KC_P    , KC_EQL   ,
+    CW_TOGG       , KC_A      , KC_S           , KC_D          , KC_F          , KC_G           ,                                              KC_H   , KC_J     , KC_K     , KC_L     , KC_SCLN , KC_QUOT  ,
+    OSM(MOD_LSFT) , KC_Z      , KC_X           , KC_C          , KC_V          , KC_B           , KC_LBRC          ,       KC_RBRC           , KC_N   , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH , KC_BSLS  ,
+    G(KC_TAB)     , C(KC_SPC) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , LT(nav,KC_SPC) , LT(mouse,KC_ESC) ,       LT(media,KC_BSPC) , KC_ENT , KC_NO    , KC_NO    , KC_NO    , SCRL_MO , OSM(MOD_MEH)
   ),
 
   [nav] = LAYOUT_universal(
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [mouse] = LAYOUT_universal(
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KBC_RST  , KBC_SAVE   , KC_NO      , KC_NO     , KC_NO      , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , G(KC_LBRC) , G(KC_RBRC) , G(KC_EQL) , G(KC_MINS) , KC_NO    ,
-    KC_NO    , MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , AML_TO   ,                                 KC_BTN2  , KC_BTN1    , KC_BTN3    , KC_NO     , KC_NO      , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           KC_NO    , KC_NO    , CPI_D100   , CPI_I100   , SCRL_DVI  , SCRL_DVD   , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , _______  , KC_NO    , _______  ,           G(KC_W)  , KC_NO    , _______    , KC_NO      , KC_NO     , KC_NO      , _______
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                             KBC_RST , KBC_SAVE   , xxx        , xxx       , xxx        , xxx    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                             xxx     , CPI_D100   , CPI_I100   , SCRL_DVI  , SCRL_DVD   , xxx    ,
+    KC_NO    , MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , AML_TO   ,                             xxx     , G(KC_LBRC) , G(KC_RBRC) , G(KC_EQL) , G(KC_MINS) , xxx    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           xxx  , G(KC_W) , KC_BTN1    , KC_BTN2    , KC_BTN3   , xxx        , xxx    ,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , _______  , KC_NO    , _______  ,           xxx  , xxx     , xxx        , xxx        , xxx       , ___        , ___
   ),
 
   [media] = LAYOUT_universal(
