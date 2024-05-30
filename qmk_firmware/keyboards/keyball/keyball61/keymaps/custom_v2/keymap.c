c3e810fd9d977def86aef3da348f69f56c65f4d4
brianliu <hong86323@gmail.com>
2024-05-31 03:02:23 +0800

    right hand navigation

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
@@ -34,11 +34,11 @@ enum Layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [base] = LAYOUT_universal(
-    KC_GRV        , KC_1         , KC_2           , KC_3          , KC_4          , KC_5    ,                           KC_6   , KC_7 , KC_8     , KC_9     , KC_0      , KC_MINS  ,
-    KC_TAB        , KC_Q         , KC_W           , KC_E          , KC_R          , KC_T    ,                           KC_Y   , KC_U , KC_I     , KC_O     , KC_P      , KC_EQL   ,
-    CW_TOGG       , KC_A         , KC_S           , KC_D          , KC_F          , KC_G    ,                           KC_H   , KC_J , KC_K     , KC_L     , KC_SCLN   , KC_QUOT  ,
-    OSM(MOD_LSFT) , KC_Z         , KC_X           , KC_C          , KC_V          , KC_B    , KC_LBRC ,       KC_RBRC , KC_N   , KC_M , KC_COMM  , KC_DOT   , KC_SLSH   , KC_BSLS  ,
-    G(KC_TAB)     , OSM(MOD_MEH) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , KC_SPC  , TO(nav) ,       KC_BSPC , KC_ENT , xxx  , KC_NO    , KC_NO    , TO(base)  , TO(media)
+    KC_GRV        , KC_1         , KC_2           , KC_3          , KC_4          , KC_5    ,                           KC_6           , KC_7 , KC_8     , KC_9     , KC_0      , KC_MINS  ,
+    KC_TAB        , KC_Q         , KC_W           , KC_E          , KC_R          , KC_T    ,                           KC_Y           , KC_U , KC_I     , KC_O     , KC_P      , KC_EQL   ,
+    CW_TOGG       , KC_A         , KC_S           , KC_D          , KC_F          , KC_G    ,                           KC_H           , KC_J , KC_K     , KC_L     , KC_SCLN   , KC_QUOT  ,
+    OSM(MOD_LSFT) , KC_Z         , KC_X           , KC_C          , KC_V          , KC_B    , KC_LBRC ,       KC_RBRC , KC_N           , KC_M , KC_COMM  , KC_DOT   , KC_SLSH   , KC_BSLS  ,
+    G(KC_TAB)     , OSM(MOD_MEH) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , KC_SPC  , TO(nav) ,       KC_BSPC , LT(nav,KC_ENT) , xxx  , KC_NO    , KC_NO    , TO(base)  , TO(media)
   ),
 
   [nav] = LAYOUT_universal(
