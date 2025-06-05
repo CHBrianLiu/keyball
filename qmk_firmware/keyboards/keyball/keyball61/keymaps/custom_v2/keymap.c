0bfd569382b10845a37eb816ac8249c33c3eaf01
brianliu <hong86323@gmail.com>
2025-06-06 00:53:29 +0800

    make the third thumb key to be ctrl-alt-cmd

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom_v2/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB        , KC_Q         , KC_W           , KC_E          , KC_R          , KC_T    ,                           KC_Y           , KC_U , KC_I     , KC_O     , KC_P      , KC_EQL   ,
     CW_TOGG       , KC_A         , KC_S           , KC_D          , KC_F          , KC_G    ,                           KC_H           , KC_J , KC_K     , KC_L     , KC_SCLN   , KC_QUOT  ,
     OSM(MOD_LSFT) , KC_Z         , KC_X           , KC_C          , KC_V          , KC_B    , KC_LBRC ,       KC_RBRC , KC_N           , KC_M , KC_COMM  , KC_DOT   , KC_SLSH   , KC_BSLS  ,
-    G(KC_TAB)     , OSM(MOD_MEH) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , KC_SPC  , TO(nav) ,       KC_BSPC , LT(nav,KC_ENT) , xxx  , KC_NO    , KC_NO    , TO(base)  , TO(media)
+    G(KC_TAB)     , OSM(MOD_MEH) , OSM(MOD_LALT)  , OSM(MOD_LGUI) , OSM(MOD_LCTL) , KC_SPC  , LAG(KC_LCTL),   KC_BSPC , LT(nav,KC_ENT) , xxx  , KC_NO    , KC_NO    , TO(base)  , TO(media)
   ),
 
   [nav] = LAYOUT_universal(
